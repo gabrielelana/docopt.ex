f1f39257aab809b054865c68cefa9e92c7cb7f56
gabriele.lana <gabriele.lana@cleancode.it>
2015-01-04 18:02:16 +0100

    Initial code to generate acceptance tests
    Once we will be able to parse and decode the file that contains the
    tests

--- test/acceptance_test.exs
+++ test/acceptance_test.exs
@@ -1,9 +1,25 @@
 defmodule Docopt.Test.Acceptance do
   use ExUnit.Case
 
+  defmodule Parser do
+    def parse(_file) do
+      [
+        %{id: 1, at_line: 0, input: "", args: "", output: ""},
+        %{id: 2, at_line: 0, input: "", args: "", output: ""},
+        %{id: 3, at_line: 0, input: "", args: "", output: ""},
+      ]
+    end
+  end
+
   @testcases "test/testcases.docopt"
 
   test "Docopt testcase file exists" do
     assert File.exists?(@testcases), "Docopt testcase file #{@testcases} not found"
+  end
+
+  for tc <- Parser.parse(@testcases) do
+    test "Acceptance test #{tc.id} at line: #{tc.at_line}" do
+      assert unquote(tc.output) == ""
+    end
   end
 end
